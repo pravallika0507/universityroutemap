4971e3ce9a53781aad5b8285ef4155688f123224
krishna-mandala <kireetigoud12@gmail.com>
2022-12-06 11:50:24 +0000

    sprint 3 db added

--- app/app.js
+++ app/app.js
@@ -7,18 +7,24 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// adding pug 
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("getdirections");
 });
 
 // Create a route for testing the db
-app.get("/db_test", function(req, res) {
+app.get("/1", function(req, res) {
     // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
+    sql = 'select * from subjects ';
+   
     db.query(sql).then(results => {
         console.log(results);
         res.send(results)
